0145a33fbf5387c5ef7db4924e13e4fa115cfbca
Alexey Zhelezov <zhelezovalexey@gmail.com>
2019-04-14 18:45:15 +0200

    Generic Type Binary Tree
    Finished implementation of the Generic Type Binary Tree, which allows
    the storage of any data type that has access to the Comparable interface

--- Programacion Varia/TPA/binaryTree/BinaryTree.java
+++ Programacion Varia/TPA/binaryTree/BinaryTree.java
@@ -1,6 +1,19 @@
 package binaryTree;
 
-public class BinaryTree<T> {
+/*
+ * Author: Alexey Zhelezov
+ * 
+ * Generic Type Binary Tree.
+ * 
+ * Makes use of the Comparable interface to compare data values which are inferred while instantiating the class.
+ * Use with the Generic Type Node class included in the package.
+ * 
+ * Repeated values are not allowed.
+ * 
+ * Pre-order, In-Order and Post-Order prints are included.
+ */
+
+public class BinaryTree<T extends Comparable<T>> {
 
 	Node<T> mainRoot;
 	
@@ -21,21 +34,67 @@ public class BinaryTree<T> {
 			return;
 		}
 		
-		if(data < currNode.data) {
+		if(data.compareTo(currNode.data) < 0) {
 			if(currNode.left == null) {
-				currNode.left = new Node(data, null, null);
+				currNode.left = new Node<T>(data, null, null);
 				return;
 			}
 			
 			insertRecursive(data, currNode.left);
-		} else if (data > currNode.data) {
+		} else if (data.compareTo(currNode.data) > 0) {
 			if(currNode.right == null) {
-				currNode.right = new Node(data, null, null);
+				currNode.right = new Node<T>(data, null, null);
 				return;
 			}
 			
 			insertRecursive(data, currNode.right);
 		}
+		
+	}
+	
+	public void printDLR() {
+		DLRrecursive(mainRoot);
+		System.out.println();
+	}
+	
+	public void printLDR() {
+		LDRrecursive(mainRoot);
+		System.out.println();
+	}
+	
+	public void printLRD() {
+		LRDrecursive(mainRoot);
+		System.out.println();
+	}
+	
+	private void DLRrecursive(Node<T> currNode) {
+		if(currNode == null)
+			return;
+		
+		System.out.print(currNode.data + " ");
+		
+		DLRrecursive(currNode.left);
+		DLRrecursive(currNode.right);
+		
+	}
+	
+	private void LDRrecursive(Node<T> currNode) {
+		if(currNode == null)
+			return;
+		
+		LDRrecursive(currNode.left);
+		System.out.print(currNode.data + " ");
+		LDRrecursive(currNode.right);
+		
+	}
+	
+	private void LRDrecursive(Node<T> currNode) {
+		if(currNode == null)
+			return;
+		
+		LRDrecursive(currNode.left);
+		LRDrecursive(currNode.right);
+		System.out.print(currNode.data + " ");
 		
 	}
 
--- Programacion Varia/TPA/binaryTree/Node.java
+++ Programacion Varia/TPA/binaryTree/Node.java
@@ -1,6 +1,14 @@
 package binaryTree;
 
-public class Node<T> {
+/*
+ * Author: Alexey Zhelezov
+ * 
+ * Generic type Node for Generic Type Binary Tree.
+ * 
+ * Implements the use of any value type that has access to the Comparable interface.
+ */
+
+public class Node<T extends Comparable<T>> {
 	
 	T data;
 	Node<T> left, right;
